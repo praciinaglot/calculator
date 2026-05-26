6dd25aa5199400793e429f525a9a66f99bb93c78
praciinaglot <naglotprachi@gmail.com>
2026-05-26 19:12:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-let display = document.getElementById("display");
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-function addToDisplay(value) {
-    display.value += value;
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    }
-
-    catch(error) {
-        display.value = "Error";
-    }
-}
-
-function clearDisplay() {
-    display.value = "";
-}
-
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
